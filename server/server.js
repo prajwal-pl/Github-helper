59ed48ed12c2169d5b9636654eead0055adeb304
Prajwal-PL <prajwalpl096@gmail.com>
2024-04-10 13:59:56 +0530

    Optimised for deployment

--- server/server.js
+++ server/server.js
@@ -8,9 +8,12 @@ import connectDB from "./db/connectDB.js";
 import session from "express-session";
 import passport from "passport";
 import "./passport/github.auth.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
+console.log(__dirname);
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
 );
@@ -19,13 +22,15 @@ app.use(passport.session());
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server testing...");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/explore", exploreRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(5000, () => {
   console.log("Server running on port: 5000");
