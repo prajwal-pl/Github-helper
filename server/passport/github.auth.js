297dbdccaae77b40da851c6b7479169691be8573
Prajwal-PL <prajwalpl096@gmail.com>
2024-04-10 14:22:53 +0530

    final url change added

--- server/passport/github.auth.js
+++ server/passport/github.auth.js
@@ -18,7 +18,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/github/callback",
+      callbackURL:
+        "https://github-helper-zebq.onrender.com/api/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       const user = await User.findOne({ username: profile.username });
